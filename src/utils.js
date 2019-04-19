791995cdf24f4d0c56369645f956a38aff2933d7
Nick Nish <nickjnish@gmail.com>
2019-04-19 14:54:08 -0700

    Small cleanup of dollarFormatter

--- src/utils.js
+++ src/utils.js
@@ -14,19 +14,17 @@ export const dollarFormatter = value => {
 
   const formatter = new Intl.NumberFormat("en-US");
   const numberParts = value.toString().split(".");
-  const wholeNumber = formatter.format(numberParts[0]);
+  const wholeNumber = formatter.format(parseFloat(numberParts[0]));
   const hundredthsNumber = numberParts[1];
-  let decimalString;
-
-  if (hundredthsNumber === "") {
-    decimalString = ".";
-  } else {
-    decimalString = hundredthsNumber ? `.${hundredthsNumber}` : "";
-  }
-
-  const numberVal = `${wholeNumber}${decimalString}`;
+  const decimalString =
+    hundredthsNumber === ""
+      ? "."
+      : hundredthsNumber
+      ? `.${hundredthsNumber}`
+      : "";
 
   // iOS Bug: Must not use a template string here
   // https://bugs.webkit.org/show_bug.cgi?id=190756
+  const numberVal = `${wholeNumber}${decimalString}`;
   return "$" + numberVal;
 };
